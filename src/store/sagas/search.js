dbeee1f39719268985a34193911e62e2c5468729
Saulo Santiago <saulodasilvasantiago@gmail.com>
2018-03-24 19:34:29 -0300

    add duck and saga to search

---
+++ src/store/sagas/search.js
@@ -0,0 +1,13 @@
+import api from 'services/api';
+import { call, put } from 'redux-saga/effects';
+
+import { Creators as SearchActions } from 'store/ducks/search';
+
+export function* search(action) {
+  try {
+    const response = yield call(api.get, `/songs?q=${action.payload.term}`);
+    yield put(SearchActions.searchSuccess(response.data));
+  } catch (error) {
+    yield put(SearchActions.searchFailure('Erro ao buscar música!'));
+  }
+}
